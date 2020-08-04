9c42519095e24349d11e5f66b8e281e43dde87c7
Adrian <adrgon@gmail.com>
2020-08-04 20:43:02 -0300

    Actualizacion de index.html main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,46 @@
 //console.log("Esto es una prueba")
+let pedido = []
+
+window.addEventListener('DOMContentLoaded', ()=>{
+
+    //var btnCancelar = document.querySelector('#btnCancelar')
+    window.btnCancelar.addEventListener('click', ()=>{
+        let seguro = confirm("Estas seguro qeu queres cancelar tu compra")
+        if(seguro) {
+            pedido = [];
+            actualizarCarrito()
+        }
+    })
+
+    let productos = document.querySelectorAll('.product')
+    //console.log(productor)
+    for(let producto of productos){
+        producto.addEventListener('click', (e)=>{
+            //console.log(e.currentTarget.dataset.producto)
+            let prod = e.currentTarget.dataset.producto
+            pedido.push(prod)
+            actualizarCarrito()
+        })
+    }
+})
+
+
+function actualizarCarrito(){
+    let html = ""
+    for(let producto of pedido){
+        html += "<li>" + producto + "</li>"
+    }
+    //console.log(html)
+    let ul = document.querySelector('#cart ul');
+    ul.innerHTML = html
+
+    let cart = document.querySelector("#cart")
+    if(pedido.length==0){
+        cart.style.backgroundColor = 'gray'
+    }else {
+        cart.style.backgroundColor = '#006'
+    }
+}
+
+
 
